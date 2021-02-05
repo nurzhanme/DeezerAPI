3fb232ecf1413a6d046e46a5a43961486c8cea13
Markus <48181660+BackInBash@users.noreply.github.com>
2021-02-05 14:46:34 +0100

    Refactoring

--- DeezerAPI.Test/Mobile/TestLyrics.cs
+++ DeezerAPI.Test/Mobile/TestLyrics.cs
@@ -1,8 +1,4 @@
 ﻿using DeezerAPI.Mobile.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
--- DeezerAPI/DeezerAPI/Public/Models/Options.cs
+++ DeezerAPI/DeezerAPI/Public/Models/Options.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DeezerAPI.Public
+﻿namespace DeezerAPI.Public
 {
     public sealed class Options
     {
