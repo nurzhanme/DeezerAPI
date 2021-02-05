4a3be6b0b33049c4e63d93c60833dabe432555d2
Markus <48181660+BackInBash@users.noreply.github.com>
2021-02-05 11:33:31 +0100

    Added UnitTest

---
+++ DeezerAPI.Test/Mobile/TestPlaylist.cs
@@ -0,0 +1,32 @@
+﻿using DeezerAPI.Mobile.Models.Playlist;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeezerAPI.Test.Mobile
+{
+    public class TestPlaylist
+    {
+        DeezerAPI.Mobile.Client client = new DeezerAPI.Mobile.Client();
+        public TestPlaylist()
+        {
+            client.GenerateSID().GetAwaiter().GetResult();
+        }
+
+        [Theory]
+        [InlineData("5915155024")]
+        public async Task PlaylistNotEmpty(string id)
+        {
+            var res = await client.GetPlaylist(id);
+            Assert.NotNull(res);
+        }
+        [Theory]
+        [InlineData("5915154904")]
+        [InlineData("5915155364")]
+        [InlineData("5915155104")]
+        public async Task TestPlaylistType(string id)
+        {
+            var res = await client.GetPlaylist(id);
+            Assert.IsType<Playlist>(res);
+        }
+    }
+}
