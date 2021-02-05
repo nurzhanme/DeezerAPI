3f8d3d525f36433396a7bbc52c592e69643dde49
Markus <48181660+BackInBash@users.noreply.github.com>
2021-02-05 11:34:06 +0100

    Updated UnitTest

--- DeezerAPI.Test/Mobile/TestTrack.cs
+++ DeezerAPI.Test/Mobile/TestTrack.cs
@@ -1,4 +1,6 @@
 ﻿using DeezerAPI.Mobile.Models.Track;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -28,5 +30,22 @@ namespace DeezerAPI.Test.Mobile
             var res = await client.GetTrack(id);
             Assert.IsType<Track>(res);
         }
+
+        [Theory]
+        [InlineData("551111062", "69002438", "86157497")]
+        public async Task TracksNotEmpty(params string[] id)
+        {
+            var res = await client.GetTracks(id.ToList());
+            Assert.NotNull(res);
+        }
+
+        [Theory]
+        [InlineData("551111062", "69002438", "86157497")]
+        public async Task TestTracksType(params string[] id)
+        {
+            var res = await client.GetTracks(id.ToList());
+            Assert.IsType<Tracks>(res);
+        }
+
     }
 }
