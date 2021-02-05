e7f48e3a1812a90a50615ccdc7b8a95efc36f7d4
Markus <48181660+BackInBash@users.noreply.github.com>
2021-02-05 10:44:15 +0100

    Added Unit Tests

---
+++ DeezerAPI.Test/Mobile/TestTrack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeezerAPI.Test.Mobile
+{
+    public class TestTrack
+    {
+        DeezerAPI.Mobile.Client client = new DeezerAPI.Mobile.Client();
+        public TestTrack()
+        {
+            client.GenerateSID().GetAwaiter().GetResult();
+        }
+
+        [Theory]
+        [InlineData("551111062")]
+        public async Task TrackNotEmpty(string id)
+        {
+            var res = await client.GetTrack(id);
+            Assert.NotNull(res);
+        }
+    }
+}
---
+++ DeezerAPI.Test/Mobile/Test_SID.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace DeezerAPI.Test.Mobile
+{
+    public class Test_SID
+    {
+        [Fact]
+        public void GetSID()
+        {
+            DeezerAPI.Mobile.Client client = new DeezerAPI.Mobile.Client();
+            client.GenerateSID().GetAwaiter().GetResult();
+            Assert.IsType<string>(client.GetSID());
+        }
+    }
+}
