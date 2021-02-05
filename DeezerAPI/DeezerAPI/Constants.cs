a14c6803f296a1f654747a0ab46d22c32a0383cd
Markus <48181660+BackInBash@users.noreply.github.com>
2021-02-05 09:20:04 +0100

    Updated UserAgent

--- DeezerAPI/DeezerAPI/Constants.cs
+++ DeezerAPI/DeezerAPI/Constants.cs
@@ -2,6 +2,6 @@
 {
     public class Constants
     {
-        public const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.58";
+        public const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.56";
     }
 }
