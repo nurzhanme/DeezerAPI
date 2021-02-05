5c469d384b8a1d35e204229d1fc78c107b5cedf7
Markus <48181660+BackInBash@users.noreply.github.com>
2021-02-05 09:37:33 +0100

    Added Response Models

---
+++ DeezerAPI/DeezerAPI/Mobile/Models/Album.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DeezerAPI.Mobile.Models.Album
+{
+    public partial class Album
+    {
+        [JsonProperty("error", NullValueHandling = NullValueHandling.Ignore)]
+        public List<object> Error { get; set; }
+
+        [JsonProperty("results", NullValueHandling = NullValueHandling.Ignore)]
+        public Results Results { get; set; }
+    }
+
+    public partial class Results
+    {
+        [JsonProperty("ALB_CONTRIBUTORS", NullValueHandling = NullValueHandling.Ignore)]
+        public AlbContributors AlbContributors { get; set; }
+
+        [JsonProperty("ALB_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? AlbId { get; set; }
+
+        [JsonProperty("ALB_PICTURE", NullValueHandling = NullValueHandling.Ignore)]
+        public string AlbPicture { get; set; }
+
+        [JsonProperty("EXPLICIT_ALBUM_CONTENT", NullValueHandling = NullValueHandling.Ignore)]
+        public ExplicitAlbumContent ExplicitAlbumContent { get; set; }
+
+        [JsonProperty("ALB_TITLE", NullValueHandling = NullValueHandling.Ignore)]
+        public string AlbTitle { get; set; }
+
+        [JsonProperty("ARTISTS", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Artist> Artists { get; set; }
+
+        [JsonProperty("ART_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ArtId { get; set; }
+
+        [JsonProperty("ART_NAME", NullValueHandling = NullValueHandling.Ignore)]
+        public string ArtName { get; set; }
+
+        [JsonProperty("ARTIST_IS_DUMMY", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? ArtistIsDummy { get; set; }
+
+        [JsonProperty("DIGITAL_RELEASE_DATE", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? DigitalReleaseDate { get; set; }
+
+        [JsonProperty("EXPLICIT_LYRICS", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ExplicitLyrics { get; set; }
+
+        [JsonProperty("NB_FAN", NullValueHandling = NullValueHandling.Ignore)]
+        public long? NbFan { get; set; }
+
+        [JsonProperty("NUMBER_DISK", NullValueHandling = NullValueHandling.Ignore)]
+        public long? NumberDisk { get; set; }
+
+        [JsonProperty("NUMBER_TRACK", NullValueHandling = NullValueHandling.Ignore)]
+        public long? NumberTrack { get; set; }
+
+        [JsonProperty("PHYSICAL_RELEASE_DATE", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? PhysicalReleaseDate { get; set; }
+
+        [JsonProperty("PRODUCER_LINE", NullValueHandling = NullValueHandling.Ignore)]
+        public string ProducerLine { get; set; }
+
+        [JsonProperty("PROVIDER_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ProviderId { get; set; }
+
+        [JsonProperty("RANK", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Rank { get; set; }
+
+        [JsonProperty("RANK_ART", NullValueHandling = NullValueHandling.Ignore)]
+        public long? RankArt { get; set; }
+
+        [JsonProperty("STATUS", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Status { get; set; }
+
+        [JsonProperty("TYPE", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Type { get; set; }
+
+        [JsonProperty("UPC", NullValueHandling = NullValueHandling.Ignore)]
+        public string Upc { get; set; }
+
+        [JsonProperty("__TYPE__", NullValueHandling = NullValueHandling.Ignore)]
+        public string ResultsType { get; set; }
+    }
+
+    public partial class AlbContributors
+    {
+        [JsonProperty("main_artist", NullValueHandling = NullValueHandling.Ignore)]
+        public List<string> MainArtist { get; set; }
+    }
+
+    public partial class Artist
+    {
+        [JsonProperty("ART_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ArtId { get; set; }
+
+        [JsonProperty("ROLE_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? RoleId { get; set; }
+
+        [JsonProperty("ARTISTS_ALBUMS_ORDER", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ArtistsAlbumsOrder { get; set; }
+
+        [JsonProperty("ART_NAME", NullValueHandling = NullValueHandling.Ignore)]
+        public string ArtName { get; set; }
+
+        [JsonProperty("ART_PICTURE", NullValueHandling = NullValueHandling.Ignore)]
+        public string ArtPicture { get; set; }
+
+        [JsonProperty("RANK", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Rank { get; set; }
+
+        [JsonProperty("LOCALES", NullValueHandling = NullValueHandling.Ignore)]
+        public Locales Locales { get; set; }
+
+        [JsonProperty("ARTIST_IS_DUMMY", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? ArtistIsDummy { get; set; }
+
+        [JsonProperty("__TYPE__", NullValueHandling = NullValueHandling.Ignore)]
+        public string Type { get; set; }
+    }
+
+    public partial class Locales
+    {
+        [JsonProperty("lang_ja-jpan", NullValueHandling = NullValueHandling.Ignore)]
+        public Lang LangJaJpan { get; set; }
+
+        [JsonProperty("lang_ja-hrkt", NullValueHandling = NullValueHandling.Ignore)]
+        public Lang LangJaHrkt { get; set; }
+
+        [JsonProperty("lang_zh-hant", NullValueHandling = NullValueHandling.Ignore)]
+        public Lang LangZhHant { get; set; }
+    }
+
+    public partial class Lang
+    {
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+    }
+
+    public partial class ExplicitAlbumContent
+    {
+        [JsonProperty("EXPLICIT_LYRICS_STATUS", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ExplicitLyricsStatus { get; set; }
+
+        [JsonProperty("EXPLICIT_COVER_STATUS", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ExplicitCoverStatus { get; set; }
+    }
+}
---
+++ DeezerAPI/DeezerAPI/Mobile/Models/Playlist.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace DeezerAPI.Mobile.Models.Playlist
+{
+    public partial class Playlist
+    {
+        [JsonProperty("error", NullValueHandling = NullValueHandling.Ignore)]
+        public List<object> Error { get; set; }
+
+        [JsonProperty("results", NullValueHandling = NullValueHandling.Ignore)]
+        public Results Results { get; set; }
+    }
+
+    public partial class Results
+    {
+        [JsonProperty("PLAYLIST_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public string PlaylistId { get; set; }
+
+        [JsonProperty("DESCRIPTION", NullValueHandling = NullValueHandling.Ignore)]
+        public string Description { get; set; }
+
+        [JsonProperty("PARENT_USERNAME", NullValueHandling = NullValueHandling.Ignore)]
+        public string ParentUsername { get; set; }
+
+        [JsonProperty("PARENT_USER_PICTURE", NullValueHandling = NullValueHandling.Ignore)]
+        public string ParentUserPicture { get; set; }
+
+        [JsonProperty("PARENT_USER_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? ParentUserId { get; set; }
+
+        [JsonProperty("PICTURE_TYPE", NullValueHandling = NullValueHandling.Ignore)]
+        public string PictureType { get; set; }
+
+        [JsonProperty("PLAYLIST_PICTURE", NullValueHandling = NullValueHandling.Ignore)]
+        public string PlaylistPicture { get; set; }
+
+        [JsonProperty("TITLE", NullValueHandling = NullValueHandling.Ignore)]
+        public string Title { get; set; }
+
+        [JsonProperty("TYPE", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Type { get; set; }
+
+        [JsonProperty("STATUS", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Status { get; set; }
+
+        [JsonProperty("USER_ID", NullValueHandling = NullValueHandling.Ignore)]
+        public long? UserId { get; set; }
+
+        [JsonProperty("DATE_ADD", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? DateAdd { get; set; }
+
+        [JsonProperty("DATE_MOD", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? DateMod { get; set; }
+
+        [JsonProperty("DATE_CREATE", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? DateCreate { get; set; }
+
+        [JsonProperty("NB_SONG", NullValueHandling = NullValueHandling.Ignore)]
+        public long? NbSong { get; set; }
+
+        [JsonProperty("NB_FAN", NullValueHandling = NullValueHandling.Ignore)]
+        public long? NbFan { get; set; }
+
+        [JsonProperty("CHECKSUM", NullValueHandling = NullValueHandling.Ignore)]
+        public string Checksum { get; set; }
+
+        [JsonProperty("HAS_ARTIST_LINKED", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? HasArtistLinked { get; set; }
+
+        [JsonProperty("IS_SPONSORED", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IsSponsored { get; set; }
+
+        [JsonProperty("IS_EDITO", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IsEdito { get; set; }
+
+        [JsonProperty("__TYPE__", NullValueHandling = NullValueHandling.Ignore)]
+        public string ResultsType { get; set; }
+    }
+}
