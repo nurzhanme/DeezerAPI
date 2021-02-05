7633dc02c57a296c645bd897d2f20bac9512154c
Markus <48181660+BackInBash@users.noreply.github.com>
2021-02-05 09:20:27 +0100

    Added Mobile API Constants

---
+++ DeezerAPI/DeezerAPI/Mobile/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace DeezerAPI.Mobile
+{
+    public class MobileConstants : Constants
+    {
+        public const string MobileAPI = "https://api.deezer.com/1.0/gateway.php";
+        public const string Input = "input=3";
+        public const string Output = "input=3";
+        public const string APIKey = "api_key=ZAIVAHCEISOHWAICUQUEXAEPICENGUAFAEZAIPHAELEEVAHPHUCUFONGUAPASUAY";
+        public const string Method = "method=";
+        public const string Sid = "sid=";
+    }
+}
---
+++ DeezerAPI/DeezerAPI/Mobile/Methods.cs
@@ -0,0 +1,17 @@
+﻿namespace DeezerAPI.Mobile
+{
+    internal class Methods
+    {
+        public const string Playlist = "playlist.getData";
+        /// <summary>
+        /// Get Deezer Track Options
+        /// </summary>
+        public const string GetTrack = "song.getData";
+        public const string GetTracks = "song.getListData";
+
+        /// <summary>
+        /// Get Deezer Album Option
+        /// </summary>
+        public const string GetAlbum = "album.getData";
+    }
+}
